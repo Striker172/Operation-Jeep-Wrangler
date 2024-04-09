36700fd957f2320f69cff989d6dbf3b20e4b3022
Striker172 <striker172113@gmail.com>
2024-04-09 08:38:58 -0500

    Changed the music library, hopefully didn't break it

--- DriveTrain/lib/Music_Speaker/examples/usage/usage.ino
+++ DriveTrain/lib/Music_Speaker/examples/usage/usage.ino
@@ -1,16 +1,17 @@
 // Example usage for Music_Speaker library by Striker.
 
 #include "Music_Speaker.h"
-
+#include "Particle.h"
+#define speakerPin D2
 // Initialize objects from the lib
-Music_Speaker music_Speaker;
+Music_Speaker music_Speaker(speakerPin);
 
 void setup() {
-    // Call functions on initialized library objects that require hardware
-    music_Speaker.begin();
 }
 
 void loop() {
     // Use the library's initialized objects and functions
-    music_Speaker.process();
+   if(music_Speaker.tuneIsOn){
+       music_Speaker.playSong(0);
+   }
 }
