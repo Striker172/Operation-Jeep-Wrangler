11072441684c7e9dde14f95f7f08eb0306673fae
tritrest <prodby.tritrest.business@gmail.com>
2024-04-23 11:29:44 -0500

    Updated Web Joystick

--- DriveTrain/src/JeepWrangler.cpp
+++ DriveTrain/src/JeepWrangler.cpp
@@ -43,6 +43,8 @@ Adafruit_NeoPixel strip(PIXEL_COUNT, PixelPin, PIXEL_TYPE);
 int setDriveSpeed(String inputString);
 //Sets the drive control aka gets the x and y values from the controller or website?
 void setDriveControlXY(const char *event, const char *data);
+//Sets the drive control aka gets the x and y values from the website
+int setCloudDriveControlXY(String inputString);
 //This gets the input from the horn switch and sets the speaker to beep, and sets the song to play
 void HornSwitch(const char *event, const char *data);
 //This will set the drive control to either remote or website and will make a beep sound
@@ -84,6 +86,7 @@ void setup() {
   Particle.function("setDriveControl(R/W)", setDriveControl);
   Particle.function("SetDriveSpeed",setDriveSpeed);
   Particle.function("SetLEDColor",setLEDColor);
+  Particle.function("SetCloudDriveControl", setCloudDriveControlXY);
   Particle.publish("DriveControl:","Remote");
   strip.begin();
 }
@@ -183,6 +186,15 @@ void setDriveControlXY(const char *event, const char *data){
   driveValue[0] = xValue;
   driveValue[1] = yValue;
 }
+
+int setCloudDriveControlXY(String inputString){
+  int xValue = inputString.substring(0, inputString.indexOf(",")).toInt(); //get the x value
+  int yValue = inputString.substring(inputString.indexOf(",")+1).toInt(); //get the y value
+  driveValue[0] = xValue;
+  driveValue[1] = yValue;
+  return 0;
+}
+
 void HornSwitch(const char *event, const char *data){
   String inputString = String(data);
   if(inputString.indexOf("ON") > -1){
